7d2e08be81db115090f4af087860178a80d0e321
hayato683 <148669953+hayato683@users.noreply.github.com>
2024-12-20 16:45:02 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $socore1 =80;
+
+  $socore2 =60;
+
+  $socore3 =55;
+
+  $socore4 =40;
+
+  $socore5 =100;
+
+  $socore6 =25;
+
+  $socore7 =80;
+
+  $socore8 =95;
+
+  $socore9 =30;
+
+  $socore10 =60;
+
+  echo ($socore1 + $socore2 + $socore3 + $socore4 + $socore5 + $socore6 + $socore7 + $socore8 + $socore9 + $socore10) / 10;
+  ?>
+
+</body>
+
+</html>
